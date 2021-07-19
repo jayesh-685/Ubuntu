d174147e3f0e11fb93e5705269c78d13ac44f809
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-07-19 23:58:34 +0530

    test1

--- CPP/lec32.cpp
+++ CPP/lec32.cpp
@@ -6,6 +6,9 @@ using namespace std;
         if (s[i] >= 'a' && s[i] <= 'z')
             s[i] -= 32;
     }
+
+    // or use transform(s.begin(), s.end(), s.begin(), ::toupper);
+    
 }
 
 void toLowerCase (string &s) {
@@ -13,10 +16,31 @@ void toLowerCase (string &s) {
         if (s[i] >= 'A' && s[i] <= 'Z')
             s[i] += 32;
     }
+
+    // or use transform(s.begin(), s.end(), s.begin(), ::tolower);
 } */
+
+int maxFreqChar (const string &s) {
+    int freqArr [26];
+    for (int i=0; i<26; i++)
+        freqArr[i] = 0;
+
+    for (int i=0; i<s.length(); i++) 
+        freqArr[s[i]-'a']++;
+
+    int maxFreq = INT_MIN;
+
+    for (int i=0; i<26; i++)
+        if (freqArr[i] > maxFreq)
+            maxFreq = freqArr[i];
+
+    return maxFreq;
+}
 
 int main () {
     string s;
     getline(cin, s);
-    cout << s << endl;
+    cout << maxFreqChar(s) << endl;
+    //cout << s << endl;
+
 }
